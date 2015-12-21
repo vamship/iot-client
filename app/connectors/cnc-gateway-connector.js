32bb42bd6e6a06b96af14e027cbc3f07afe916dc
vamship <vamshi.ponnapalli@gmail.com>
2015-12-21 15:14:42 -0500

    Added basic support for gateway actions to the cnc gateway connector

--- app/connectors/cnc-gateway-connector.js
+++ app/connectors/cnc-gateway-connector.js
@@ -3,11 +3,8 @@
 
 var _util = require('util');
 var _q = require('q');
-var _serialport = require('serialport');
-var SerialPort = _serialport.SerialPort;
-//SerialPort = require('../../test/mock-serial-port');
 var Connector = require('iot-client-lib').Connector;
-var EbaraPumpParser = require('./io/ebara-pump-parser');
+var CommandExecutor = require('../utils/startup-helper');
 
 var DEFAULT_REQUEST_ID = 'na';
 
@@ -21,9 +18,36 @@ var DEFAULT_REQUEST_ID = 'na';
  */
 function CncGatewayConnector(id) {
     CncGatewayConnector.super_.call(this, id)
+    this._commandExecutor = null;
+    this._cloudLogger = this._createCloudLogger();
 }
 
 _util.inherits(CncGatewayConnector, Connector);
+
+/**
+ * @class CncGatewayConnector
+ * @method _createCloudLogger
+ * @private
+ */
+Controller.prototype._createCloudLogger = function() {
+    var logger = {};
+    [ 'info', 'warn', 'error' ].forEach(function(methodName) {
+        logger[methodName] = function(message, requestId) {
+            if(message instanceof Array) {
+                message = _util.format.apply(_util, message);
+            }
+            var payload = {
+                requestId: requestId || 'na',
+                qos: (methodName === 'info')? 0:1,
+                message: '[' + methodName + '] [' + requestId + '] ' + message.toString()
+            }
+            this.emit(CncGatewayConnector.super_.LOG_EVENT, payload);
+        }.bind(this);
+    }.bind(this));
+
+    return logger;
+};
+
 
 /**
  * @class CncGatewayConnector
@@ -33,6 +57,11 @@ _util.inherits(CncGatewayConnector, Connector);
 CncGatewayConnector.prototype._start = function() {
     this._logger.info('Initializing connector');
 
+    // TODO: Review this is the correct place to do the
+    // initialization.
+    if(!this._commandExecutor) {
+        this._commandExecutor = new CommandExecutor(this._logger);
+    }
     var def = _q.defer();
     def.resolve();
 
@@ -71,7 +100,43 @@ CncGatewayConnector.prototype.addLogData = function(data) {
  */
 CncGatewayConnector.prototype.addData = function(data, requestId) {
     requestId = requestId || DEFAULT_REQUEST_ID;
-    console.log(data);
+    if(!data || typeof data !== 'object') {
+        this._logger.warn('Data payload was in an invalid format: [%s]', data);
+        return;
+    }
+    if(typeof data.command !== 'string') {
+        this._logger.warn('Data does not define a valid command: [%s]', data.command);
+        return;
+    }
+
+    var data = null;
+    var command = data.command.toLowerCase();
+    this._logger.debug('Processing command from cloud: [%s]', data.command);
+    switch(command) {
+        case 'reboot':
+            this._commandExecutor.reboot(requestId).then(function(){
+                this._cloudLogger.info([ 'Reboot scheduled' ], requestId);
+            }.bind(this), function(err) {
+                this._cloudLogger.info([ 'Error scheduling reboot: [%s]', err ], requestId);
+            }.bind(this));
+            break;
+        case 'disable_local_network':
+            this._commandExecutor.enableHostAP(requestId).then(function(){
+                this._cloudLogger.info([ 'Local AP daemon enabled on startup' ], requestId);
+            }.bind(this), function(err) {
+                this._cloudLogger.info([ 'Error enabling local AP daemon on startup: [%s]', err ], requestId);
+            }.bind(this));
+            this._commandExecutor.enableDhcp(requestId).then(function(){
+                this._cloudLogger.info([ 'Local DHCP daemon enabled on startup' ], requestId);
+            }.bind(this), function(err) {
+                this._cloudLogger.info([ 'Error enabling local DHCP daemon on startup: [%s]', err ], requestId);
+            }.bind(this));
+            break;
+        default:
+            this._logger.warn('Unrecognized command: [%s]', data.command);
+            this._cloudLogger.warn([ 'Unrecognized command: [%s]', data.command ], requestId);
+            break;
+    }
 };
 
 module.exports = CncGatewayConnector;
